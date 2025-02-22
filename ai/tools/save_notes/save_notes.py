806365b45088b3275737a4ee2d9d43e19cc39919
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-02-23 07:39:01 +0800

    updated notes

--- ai/tools/save_notes/save_notes.py
+++ ai/tools/save_notes/save_notes.py
@@ -1,8 +1,10 @@
 from ai.config import env
+from langchain_core.tools import tool
 
 
+@tool
 def save_to_notes(notes: str):
-    """Save the notes to the file when requested. Use this tool only when requested. Do not use this tool unless asked to save notes."""
+    """Save the notes to the file only when requested. Use this tool only on request. Do not use this tool unless asked to save notes."""
     with open(env.notes_path, "a") as f:
         f.write(notes)
         f.write("\n")
