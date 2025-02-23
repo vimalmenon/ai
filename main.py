315c4dcd75123ee4467b8a72d22fbf3487a708f1
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-02-23 21:29:18 +0800

    updated Main

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 from ai.agents.simple_agents import agent_executor
-from langchain_core.messages import HumanMessage
+from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
+from uuid import uuid4
 
 
 def run():
-    config = {"configurable": {"thread_id": "abc123"}}
-    messages = []
+    config = {"configurable": {"thread_id": str(uuid4())}}
+    messages = [
+        SystemMessage(content="Your name is Elara, You are helpful AI assistant")
+    ]
     print("Hello!! My name is Elara, How can i help you today?")
     while user_input := input("You: "):
         if user_input.lower() == "bye":
             print("Elara: Bye! Have a nice day!")
             break
+        messages.append(HumanMessage(content=user_input))
         for step in agent_executor.stream(
-            {"messages": messages.append(HumanMessage(content=user_input))},
+            {"messages": messages},
             config,
             stream_mode="values",
         ):
+            messages.append(AIMessage(content=step["messages"][-1].content))
             step["messages"][-1].pretty_print()
