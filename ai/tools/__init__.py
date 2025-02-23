11de2f31d33b2daf0e19375c50a90e0e381f322f
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-02-24 06:57:38 +0800

    updated README

--- ai/tools/__init__.py
+++ ai/tools/__init__.py
@@ -1,4 +1,4 @@
-from ai.tools.save_notes.save_notes import save_to_notes
 from ai.tools.multiply.multiply import multiply
+from ai.tools.save_notes.save_notes import save_to_notes
 
 tools = [save_to_notes, multiply]
