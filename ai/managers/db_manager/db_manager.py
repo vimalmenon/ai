f00598e43a4a5aebd2951a787ac83ce59f194468
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-03-08 07:37:31 +0800

    addedc eception and other changes

--- ai/managers/db_manager/db_manager.py
+++ ai/managers/db_manager/db_manager.py
@@ -1,9 +1,8 @@
-import boto3
+import uuid
+from datetime import datetime
 
 from ai.config.env import env
 from ai.managers.aws.session import Session
-
-dynamodb = boto3.resource("dynamodb")
 
 
 class DbManager:
@@ -15,8 +14,18 @@ class DbManager:
     def create(self):
         return self.table.creation_date_time
 
-    def add_item(self):
-        pass
+    def add_item(self, data):
+        return self.table.put_item(
+            Item={
+                "id": str(uuid.uuid4()),
+                "app": "vm#vim",
+                "describe": data.describe,
+                "command": data.command,
+                "language": data.language,
+                "tags": data.tags,
+                "creation_date": datetime.now().isoformat(),
+            }
+        )
 
     def remove_item(self):
         pass
