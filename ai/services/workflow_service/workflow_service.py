c4d4a1d0c621b6325549bbc93978b5608c87dc0b
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-03-09 04:14:10 +0800

    Working on Workflow

--- ai/services/workflow_service/workflow_service.py
+++ ai/services/workflow_service/workflow_service.py
@@ -3,6 +3,12 @@ class WorkflowService:
     def get_workflows(self):
         return [
             {
+                "name": "TopicWorkflow",
+                "detail": "This workflow generate topic for blogs",
+                "agents": [{"name": "topic_writer", "type": "agent"}],
+                "connection": {"START": ["topic_writer"], "topic_writer": ["END"]},
+            },
+            {
                 "name": "BlogWorkflow",
                 "detail": "This workflow help to create blogs",
                 "agents": [
@@ -10,7 +16,8 @@ class WorkflowService:
                     {"name": "blog_critique", "type": "agent"},
                     {"name": "supervisor", "type": "supervisor"},
                 ],
-            }
+                "workflow": {},
+            },
         ]
 
     def add_workflow(self):
