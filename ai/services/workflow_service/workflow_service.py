7bf34ef659ea3b01ac96ed313fc57249de9a496e
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-03-11 22:25:12 +0800

    create Workflow services

--- ai/services/workflow_service/workflow_service.py
+++ ai/services/workflow_service/workflow_service.py
@@ -6,8 +6,18 @@ class WorkflowService:
                 "id": "9454830b-6daf-47f7-8fca-13d966660cf1",
                 "name": "TopicWorkflow",
                 "detail": "This workflow generate topic for blogs",
-                "agents": [{"name": "topic_writer", "type": "agent"}],
-                "connection": {"START": ["topic_writer"], "topic_writer": ["END"]},
+                "agents": [
+                    {
+                        "name": "topic_writer",
+                        "type": "agent",
+                        "prompt": (
+                            "You have to come up with 10 titles for the blogs based on topic."
+                            "Give the output as a list."
+                            "Topic should be innovative and interesting"
+                        ),
+                    }
+                ],
+                "connections": {"START": ["topic_writer"], "topic_writer": ["END"]},
             },
             {
                 "id": "18aad31b-ef41-4853-a97a-17fd8647574c",
@@ -18,7 +28,7 @@ class WorkflowService:
                     {"name": "blog_critique", "type": "agent"},
                     {"name": "supervisor", "type": "supervisor"},
                 ],
-                "workflow": {
+                "connections": {
                     "START": ["supervisor"],
                     "supervisor": ["blog_writer", "blog_critique", "END"],
                     "blog_writer": ["supervisor"],
