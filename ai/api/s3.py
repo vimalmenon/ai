49665dbdc4205af38d080d9cc15e8fc031a084cd
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-03-13 20:28:05 +0800

    updated s3 api

--- ai/api/s3.py
+++ ai/api/s3.py
@@ -8,10 +8,10 @@ router = APIRouter()
 @router.get("/", tags=["S3"])
 async def get_items():
     """Get data from S3 Bucket data"""
-    return S3Service().get_items()
+    return {"data": S3Service().get_items()}
 
 
 @router.get("/read_item/{item}", tags=["S3"])
 async def read_items(item: str):
     """Read the data based on item key provided"""
-    return S3Service().read_item(item)
+    return {"data": S3Service().read_item(item)}
