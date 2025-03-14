8ca3c33acc8e0ad42aecf481d4c210861edd26c0
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-03-14 13:31:08 +0800

    added tools to service

--- ai/services/tool_service/tool_service.py
+++ ai/services/tool_service/tool_service.py
@@ -5,11 +5,11 @@ class ToolService:
             {
                 "id": "91fc80be-b91c-40c7-baa4-8f7508290cb9",
                 "name": "Save Notes",
-                "tool": "save_to_notes",
+                "tool_name": "save_to_notes",
             },
             {
                 "id": "6afa1343-2dd7-4ead-a96d-642731f6d86f",
                 "name": "Upload To S3 Bucket",
-                "tool": "save_to_s3",
+                "tool_name": "save_to_s3",
             },
         ]
