affbab0b701e92f476a1fa3c6d08bcd0e5ccc267
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-03-14 13:34:28 +0800

    save to db

---
+++ ai/tools/save_to_db/save_to_db.py
@@ -0,0 +1,6 @@
+def save_to_db(item):
+    """
+    Use this tool to save to DB
+    Use this tool only on request.
+    """
+    print(item)
--- ai/tools/save_to_s3/save_to_s3.py
+++ ai/tools/save_to_s3/save_to_s3.py
@@ -4,7 +4,7 @@ from langchain_core.tools import tool
 @tool
 def save_to_s3(item: str):
     """
-    Use this tool to Upload the item to S3 bucket
+    Use this tool to upload the item to S3 bucket
     Use this tool only on request.
     """
     print(item)
