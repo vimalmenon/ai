3263f9a62b2c3085f1fb041d5177cc708de6cabe
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-03-14 13:05:07 +0800

    remove multiply

--- ai/tools/__init__.py
+++ ai/tools/__init__.py
@@ -1,11 +1,9 @@
-from ai.tools.multiply.multiply import multiply
 from ai.tools.save_notes.save_notes import save_to_notes
 from ai.tools.save_to_s3.save_to_s3 import save_to_s3
 
-tools = [save_to_notes, multiply, save_to_s3]
+tools = [save_to_notes, save_to_s3]
 
 __all__ = [
-    "multiply",
     "save_to_notes",
     "save_to_s3",
 ]
