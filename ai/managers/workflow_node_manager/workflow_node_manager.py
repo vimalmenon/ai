48778353e26841765b7a35bde6e0ea8107c4bf44
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-05-01 19:24:29 +0800

    working on workflow

--- ai/managers/workflow_node_manager/workflow_node_manager.py
+++ ai/managers/workflow_node_manager/workflow_node_manager.py
@@ -1,52 +1,49 @@
 from ai.common import ClientError
 from ai.managers.workflow_manager.workflow_manager import WorkflowManager
-from ai.model import WorkflowNodeRequest
+from ai.model import WorkflowModel, WorkflowNodeRequest
 from ai.utilities import generate_uuid
 
 
 class WorkflowNodeManager:
     def create_workflow_node(self, wf_id: str, body):
         """Create the workflow node"""
-        workflow = WorkflowManager().get_workflow_by_id(wf_id)
-        if not workflow:
-            raise ClientError(
-                status_code=404,
-                detail=f"Workflow with ID {wf_id} not found.",
-            )
+        workflow = self.__validate_and_return_workflow(wf_id)
         uuid = generate_uuid()
         workflow.nodes[uuid] = WorkflowNodeRequest.from_dict(body)
         return WorkflowManager().update_workflow_node(wf_id, workflow.nodes)
 
-    def delete_workflow_nodes(self, wf_id, id):
+    def delete_workflow_nodes(self, wf_id: str, id: str):
         """Delete the workflow node by ID"""
-        workflow = WorkflowManager().get_workflow_by_id(wf_id)
-        if not workflow:
-            raise ClientError(
-                status_code=404,
-                detail=f"Workflow with ID {wf_id} not found.",
-            )
-        node = workflow.nodes.get(id)
-        if not node:
-            raise ClientError(
-                status_code=404,
-                detail=f"Workflow node with ID {id} not found.",
-            )
+        workflow = self.__validate_and_return_workflow(wf_id)
+        self.__validate_and_return_node(workflow, id)
         del workflow.nodes[id]
         return WorkflowManager().update_workflow_node(wf_id, workflow.nodes)
 
     def update_workflow_node(self, wf_id, id, data):
         """Update the workflow node by ID"""
+        workflow = self.__validate_and_return_workflow(wf_id)
+        self.__validate_and_return_node(workflow, id)
+        workflow.nodes[id] = WorkflowNodeRequest.from_dict(data)
+        return WorkflowManager().update_workflow_node(wf_id, workflow.nodes)
+
+    def __validate_and_return_workflow(self, wf_id: str) -> WorkflowModel:
+        """Validate the workflow"""
         workflow = WorkflowManager().get_workflow_by_id(wf_id)
         if not workflow:
             raise ClientError(
                 status_code=404,
                 detail=f"Workflow with ID {wf_id} not found.",
             )
+        return workflow
+
+    def __validate_and_return_node(
+        self, workflow: WorkflowModel, id: str
+    ) -> WorkflowNodeRequest:
+        """Validate the workflow node"""
         node = workflow.nodes.get(id)
         if not node:
             raise ClientError(
                 status_code=404,
                 detail=f"Workflow node with ID {id} not found.",
             )
-        workflow.nodes[id] = WorkflowNodeRequest.from_dict(data)
-        return WorkflowManager().update_workflow_node(wf_id, workflow.nodes)
+        return node
