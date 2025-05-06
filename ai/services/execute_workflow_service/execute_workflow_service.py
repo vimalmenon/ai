8b093d0e77156ec483615365705a6ffd5b0d9b81
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-05-06 21:28:20 +0800

    Adds agent node execution with LLM integration
    Introduces logic to handle agent-type workflow nodes using a React agent created with the LLM. Logs the final message content from the agent's response for visibility.
    
    Improves maintainability by refining node retrieval and execution logic.

--- ai/services/execute_workflow_service/execute_workflow_service.py
+++ ai/services/execute_workflow_service/execute_workflow_service.py
@@ -1,4 +1,12 @@
+from logging import getLogger
+
+from langgraph.prebuilt import create_react_agent
+
+from ai.llms import deepseek_llm
+from ai.model import WorkflowNodeRequest
 from ai.services.workflow_service.workflow_service import WorkflowService
+
+logger = getLogger(__name__)
 
 
 class ExecuteWorkflowService:
@@ -8,9 +16,23 @@ class ExecuteWorkflowService:
     def execute(self):
         item = WorkflowService().get_workflow_by_id(self.id)
         if item:
-            nodes = item.get("nodes", {})
-            [self.__execute_node(node) for id, node in nodes.items()]
+            nodes = item.nodes
+            for _id, node in nodes.items():
+                self.__execute_node(node)
         return {"item": None}
 
-    def __execute_node(self, node):
-        print(node)
+    def __execute_node(self, node: WorkflowNodeRequest):
+        if node.type == "agent":
+            self.__execute_agent_node(node)
+
+    def __execute_agent_node(self, node: WorkflowNodeRequest):
+        agent_llm = create_react_agent(
+            model=deepseek_llm,
+            tools=[],
+            name="resume_critique",
+            prompt="You are a helpful assistant",
+        )
+        result = agent_llm.invoke(
+            {"messages": [{"role": "user", "content": node.prompt}]}
+        )
+        logger.warning(result["messages"][-1].content)
