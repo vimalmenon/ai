adb64df75d7d91a60cfe70327e301539479ba21d
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-06-02 07:02:17 +0800

    Enhance LLMExecuteService by adding logging and refactoring execution flow

--- ai/services/llm_execute/llm_execute_service.py
+++ ai/services/llm_execute/llm_execute_service.py
@@ -1,3 +1,5 @@
+from logging import getLogger
+
 from langgraph.prebuilt import create_react_agent
 
 from ai.model import (
@@ -6,13 +8,20 @@ from ai.model import (
 from ai.services.llm_service.llm_service import LLmService
 from ai.services.tool_service.tool_service import ToolService
 
+logger = getLogger(__name__)
+
 
 class LLMExecuteService:
 
     def execute(self, node: WorkflowNodeRequest):
-        return create_react_agent(
+        agent_llm = create_react_agent(
             model=LLmService(llm=node.llm).get_llm(),
             tools=[ToolService().get_tool_func(tool) for tool in node.tools],
             name=node.name,
             prompt="You are a helpful assistant",
         )
+        result = agent_llm.invoke(
+            {"messages": [{"role": "user", "content": node.prompt}]}
+        )
+        logger.info(result)
+        return result
