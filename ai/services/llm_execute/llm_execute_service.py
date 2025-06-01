8911ff53e5a535b137e2f377fbcd7b09e473b6d0
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-06-02 07:09:26 +0800

    Refactor LLMExecuteService to enhance response parsing and logging

--- ai/services/llm_execute/llm_execute_service.py
+++ ai/services/llm_execute/llm_execute_service.py
@@ -1,5 +1,6 @@
 from logging import getLogger
 
+from langchain_core.messages.ai import AIMessage
 from langgraph.prebuilt import create_react_agent
 
 from ai.model import (
@@ -7,6 +8,7 @@ from ai.model import (
 )
 from ai.services.llm_service.llm_service import LLmService
 from ai.services.tool_service.tool_service import ToolService
+from ai.utilities import created_date, generate_uuid
 
 logger = getLogger(__name__)
 
@@ -23,5 +25,18 @@ class LLMExecuteService:
         result = agent_llm.invoke(
             {"messages": [{"role": "user", "content": node.prompt}]}
         )
-        logger.info(result)
-        return result
+        logger.warning(self.__parse_response(result["messages"][-1]))
+        return self.__parse_response(result["messages"][-1])
+
+    def __parse_response(self, response: AIMessage) -> dict[str, str]:
+        response_metadata = response.response_metadata
+        return {
+            "id": generate_uuid(),
+            "content": str(response.content) or "",
+            "model_name": response_metadata.get("model_name", ""),
+            "name": response.name or "",
+            "total_tokens": response_metadata.get("token_usage", {}).get(
+                "total_tokens", ""
+            ),
+            "created_at": created_date(),
+        }
