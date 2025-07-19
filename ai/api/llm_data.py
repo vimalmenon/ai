a2294acf1c99cb63c5a5f1c2e39aea3b3e4e1750
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-07-19 20:33:22 +0800

    Workinf oon get EB

--- ai/api/llm_data.py
+++ ai/api/llm_data.py
@@ -1,8 +1,11 @@
 from fastapi import APIRouter
 
+from ai.services import DbService
+
 router = APIRouter()
 
 
-@router.get("/db", tags=["llm_data"])
-async def get_llm_data_from_db():
-    pass
+@router.get("/db/{wf_id}", tags=["llm_data"])
+async def get_llm_data_from_db(wf_id: str):
+    data = DbService().get_by_id(wf_id)
+    return {"data": data}
