852f12832ef16f98e47899ec5369abbb60aa82dc
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-07-23 21:22:11 +0800

    added blog service and modal

--- ai/model/blog.py
+++ ai/model/blog.py
@@ -1,7 +1,33 @@
 from ai.model.base_model import Base
 
 
+class BlogContent(Base):
+    id: str
+    title: str
+    url: str
+    content: str
+
+    def to_dict(self) -> dict:
+        """Convert the object to a dictionary."""
+        return {
+            "id": self.id,
+            "title": self.title,
+            "url": self.url,
+            "content": self.content,
+        }
+
+
 class BlogTopic(Base):
     id: str
     title: str
     url: str
+    blogs: list[BlogContent] = []
+
+    def to_dict(self) -> dict:
+        """Convert the object to a dictionary."""
+        return {
+            "id": self.id,
+            "title": self.title,
+            "url": self.url,
+            "blogs": [blog.to_dict() for blog in self.blogs],
+        }
---
+++ ai/services/blog_service/blog_service.py
@@ -0,0 +1,2 @@
+class BlogService:
+    pass
