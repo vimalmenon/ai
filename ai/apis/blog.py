567fe9db02e7c38a0191bcf61013c75324ef4263
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-07-26 00:36:22 +0800

    removed shared tasks

--- ai/apis/blog.py
+++ ai/apis/blog.py
@@ -1,4 +1,3 @@
-from celery import shared_task
 from fastapi import APIRouter
 
 from ai.services import BlogService
@@ -10,8 +9,3 @@ router = APIRouter()
 async def get_blog_topic():
     result = BlogService().get_topics()
     return {"data": result}
-
-
-@shared_task
-def test_celery():
-    pass
