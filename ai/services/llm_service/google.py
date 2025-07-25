ab8122f25713827daafbd4259de778fe1778b3e8
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-07-25 19:15:22 +0800

    more refactor

--- ai/services/llm_service/google.py
+++
@@ -1,5 +0,0 @@
-from langchain_google_genai import ChatGoogleGenerativeAI
-
-from ai.config import env
-
-google_llm = ChatGoogleGenerativeAI(temperature=env.temperature, model="gemini-1.5-pro")
--- ai/services/llm_service/openai.py
+++
@@ -1,8 +0,0 @@
-from langchain_openai import ChatOpenAI
-
-from ai.config import env
-
-openai_llm = ChatOpenAI(
-    model="gpt-4o",
-    temperature=env.temperature,
-)
