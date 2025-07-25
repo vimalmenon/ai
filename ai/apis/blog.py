349c4282ee49d797028512e1320f3c4d3cd4af78
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-07-26 00:03:07 +0800

    some refactor

--- ai/apis/blog.py
+++ ai/apis/blog.py
@@ -1,3 +1,4 @@
+from celery import shared_task
 from fastapi import APIRouter
 
 from ai.services import BlogService
@@ -9,3 +10,8 @@ router = APIRouter()
 async def get_blog_topic():
     result = BlogService().get_topics()
     return {"data": result}
+
+
+@shared_task
+def test_celery():
+    pass
