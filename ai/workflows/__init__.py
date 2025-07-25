988373351fee77e709cbac86190342d3abdedba0
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-07-25 19:02:11 +0800

    remove chabot builder

--- ai/workflows/__init__.py
+++ ai/workflows/__init__.py
@@ -1,4 +1,3 @@
-from ai.workflows.chat_bot_builder.chat_bot_builder import ChatBotBuilder
 from ai.workflows.execute_workflow.execute_workflow import ExecuteWorkflow
 from ai.workflows.topic_workflow.topic_workflow import TopicWorkflow
 from ai.workflows.website_content_builder.website_content_builder import (
@@ -6,7 +5,6 @@ from ai.workflows.website_content_builder.website_content_builder import (
 )
 
 __all__ = [
-    "ChatBotBuilder",
     "WebsiteContentBuilder",
     "TopicWorkflow",
     "ExecuteWorkflow",
