2954d340f2d4b19e47504c9aa9848e82ed325f37
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-08-02 21:19:19 +0800

    feat: add tests for creating and retrieving workflows in WorkflowManager

--- ai/tests/managers/workflow_manager/test_workflow_manager.py
+++ ai/tests/managers/workflow_manager/test_workflow_manager.py
@@ -1,6 +1,20 @@
+from ai.managers.workflow_manager.workflow_manager import WorkflowManager
 from ai.tests.factory.modal.workflow import FactoryWorkflowModel
 
 
-def test_workflow_model_factory():
-    workflow = FactoryWorkflowModel()
+def test_create_and_read_workflow_by_id(dynamodb_mock, setup_environment):
+    workflow = WorkflowManager().create_workflow(FactoryWorkflowModel())
     assert workflow.id is not None
+    assert WorkflowManager().get_workflow_by_id(workflow.id) == workflow
+
+
+def test_get_workflows(dynamodb_mock, setup_environment):
+    WorkflowManager().create_workflow(FactoryWorkflowModel())
+    workflows = WorkflowManager().get_workflows()
+    assert len(workflows) == 1
+
+
+def test_delete_workflow_by_id(dynamodb_mock, setup_environment):
+    workflow = WorkflowManager().create_workflow(FactoryWorkflowModel())
+    WorkflowManager().delete_workflows_by_id(workflow.id)
+    assert WorkflowManager().get_workflow_by_id(workflow.id) is None
