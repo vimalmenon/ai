afdb00768e98ad2c20ef351a2ee29aa941b0cb5f
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-08-05 12:37:34 +0800

    fix: add missing import statements in test_link_service.py

--- ai/tests/services/test_link_service.py
+++ ai/tests/services/test_link_service.py
@@ -1,3 +1,6 @@
+import pytest
+
+from ai.exceptions.exceptions import ClientError
 from ai.services import LinkService
 from ai.tests.factory.link import FactoryLinkGroupSlim, FactoryLinkSlim
 
@@ -12,3 +15,8 @@ def test_link_service_create_link(dynamodb_mock) -> None:
     result = LinkService().create_link(result[0].id, FactoryLinkSlim.build())
     assert len(result) == 1
     assert len(result[0].links) == 1
+
+
+def test_link_service_create_link_with_exception(dynamodb_mock) -> None:
+    with pytest.raises(ClientError):
+        LinkService().create_link("non_existent_group_id", FactoryLinkSlim.build())
