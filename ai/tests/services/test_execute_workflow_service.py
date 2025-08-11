466da9970e90ebd921373fc9b61aa0f6989a136f
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-08-11 10:20:59 +0800

    refactor: Format test function signatures for improved readability

--- ai/tests/services/test_execute_workflow_service.py
+++ ai/tests/services/test_execute_workflow_service.py
@@ -10,7 +10,9 @@ from ai.tests.factory.workflow import (
 )
 
 
-def test_execute_workflow_service_raises_error_with_invalid_workflow_id(faker, dynamodb_mock) -> None:
+def test_execute_workflow_service_raises_error_with_invalid_workflow_id(
+    faker, dynamodb_mock
+) -> None:
     service = ExecuteWorkflowService()
     with pytest.raises(ClientError):
         service.create_executed_workflow(
@@ -18,7 +20,9 @@ def test_execute_workflow_service_raises_error_with_invalid_workflow_id(faker, d
         )
 
 
-def test_execute_workflow_service_raises_error_when_workflow_not_complete(dynamodb_mock) -> None:
+def test_execute_workflow_service_raises_error_when_workflow_not_complete(
+    dynamodb_mock,
+) -> None:
     service = WorkflowService().create_workflow(FactoryWorkflowSlimModel.build())
     with pytest.raises(ClientError) as msg:
         ExecuteWorkflowService().create_executed_workflow(
@@ -27,7 +31,9 @@ def test_execute_workflow_service_raises_error_when_workflow_not_complete(dynamo
     assert str(msg.value.detail) == f"Workflow with ID {service.id} is not complete."
 
 
-def test_execute_workflow_service_successfully_creates_executed_workflow(dynamodb_mock) -> None:
+def test_execute_workflow_service_successfully_creates_executed_workflow(
+    dynamodb_mock,
+) -> None:
     workflow = WorkflowService().create_workflow(FactoryWorkflowSlimModel.build())
     update_data = FactoryUpdateWorkflowRequest.build()
     update_data.complete = False
